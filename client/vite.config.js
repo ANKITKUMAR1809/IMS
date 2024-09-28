36b909650b5cdb30eeb77ffc1b5220ddca2b37d7
ANKITKUMAR1809 <skwedsak143@gmail.com>
2024-09-29 05:27:38 +0530

    added server link

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy:{
-      '/api':'http://localhost:5000'
+      '/api':'https://ims-yxa0.onrender.com'
     }
   },
   plugins: [react()],
