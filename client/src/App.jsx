3dfb5fc504bdc35acd2b29393300827f4e051959
ANKITKUMAR1809 <skwedsak143@gmail.com>
2024-09-29 16:37:11 +0530

    delete importing line of app.css because file deleted

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Layout from './pages/Layout'
 import Home from './pages/Home'
